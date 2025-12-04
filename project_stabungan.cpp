4542d3d19ae98f5303c86f6d4c9165a0a7f9310e
nurakbarputraperdana13-a11y <nurakbarputraperdana13@gmail.com>
2025-12-04 14:06:33 +0700

    Add files via upload

---
+++ project_stabungan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// fungsi pin :
+
+// fungsi masukin tabungan sama tglnya :
+
+// riwayat masukin  tabungan :
+
+// fungsi riwayat total saldo :
+
+
+
+
+int main()
+{
+ // fungsi utama 
+    return 0;
+}
